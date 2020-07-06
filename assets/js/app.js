f6e0432b2c21b9028665f444ae3c805c67a9fad0
Viktor <vitostagan@mail.ru>
2020-07-06 09:24:11 +0300

    Fix design

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,6 +28,7 @@ document.querySelector('#promokodeID-reebok').addEventListener(
     const code = event.target.innerText;
     await navigator.clipboard.writeText(code);
     alert(`Промокод ${code} скопирован!`);
+    location.href=`https://www.reebok.ru`;
   }
 );
 
@@ -37,62 +38,6 @@ document.querySelector('#promokodeID-adidas').addEventListener(
     const code = event.target.innerText;
     await navigator.clipboard.writeText(code);
     alert(`Промокод ${code} скопирован!`);
+    location.href=`https://www.adidas.ru`;
   }
 );
-
-
-// For Reebok button
-var $ButOpenSiteReebok = $('#Reebok'),
-    $DialogPromoReebok = $('#dialog-reebok');
-
-$ButOpenSiteReebok.on('click', function() {
-    $DialogPromoReebok[0].showModal();
-});
-
-$('#cancelDialogReebok').on('click', function() {
-    $DialogPromoReebok[0].close();
-});
-
-var transition;
- 
-$ButOpenSiteReebok.on('click', function() {
-    $DialogPromoReebok[0].showModal();
-    transition = setTimeout(function() {
-        $DialogPromoReebok.addClass('dialog-scale');
-    }, 0.5);
-});
-
-$('#cancelDialogReebok').on('click', function() {
-    $DialogPromoReebok[0].close();
-    $DialogPromoReebok.removeClass('dialog-scale');
-    clearTimeout(transition);
-});
-
-
-
-// For Adidas button
-var $ButOpenSiteAdidas = $('#Adidas'),
-    $DialogPromoAdidas = $('#dialog-adidas');
-
-$ButOpenSiteAdidas.on('click', function() {
-    $DialogPromoAdidas[0].showModal();
-});
-
-$('#cancelDialogAdidas').on('click', function() {
-    $DialogPromoAdidas[0].close();
-});
-
-var transition;
- 
-$ButOpenSiteAdidas.on('click', function() {
-    $DialogPromoAdidas[0].showModal();
-    transition = setTimeout(function() {
-        $DialogPromoAdidas.addClass('dialog-scale');
-    }, 0.5);
-});
-
-$('#cancelDialogAdidas').on('click', function() {
-    $DialogPromoAdidas[0].close();
-    $DialogPromoAdidas.removeClass('dialog-scale');
-    clearTimeout(transition);
-});
